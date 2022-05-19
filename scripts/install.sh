3451e543305d923a379b7674d1c98133d2167c53
David Nussio <david.nussio@gmail.com>
2022-05-19 08:02:40 +0200

    chore: update system with topgrade

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -i
 
-[[ $- != *i* ]] && { printf "🔥 Rerun in interactive mode: $ bash -i ./install.sh\n"; exit 1; }
+[[ $- != *i* ]] && {
+    printf "🔥 Rerun in interactive mode: $ bash -i ./install.sh\n"
+    exit 1
+}
 
 INSTALL_DEV_GUI_TOOLS=n
 LOGFILE=install.log
@@ -30,17 +33,17 @@ command -v sudo >/dev/null 2>&1 || { alias sudo='dummySudo'; }
 
 # Ask for the administrator password upfront
 printf "👑 Electing user\n"
-sudo -v 
+sudo -v
 
 # printf "🔄 Updating system\n"
-sudo apt update &>> $LOGFILE && sudo apt full-upgrade -y &>> $LOGFILE
+sudo apt update &>>$LOGFILE && sudo apt full-upgrade -y &>>$LOGFILE
 
 # Configure watches
 printf "🧬 Configure sysctl inotify\n"
 grep -q "fs.inotify.max_user_watches" /etc/sysctl.conf
 if [[ $? != 0 ]]; then
-    echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf &>> $LOGFILE
-    sudo sysctl -p &>> $LOGFILE
+    echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf &>>$LOGFILE
+    sudo sysctl -p &>>$LOGFILE
 fi
 
 # Install base packages
@@ -52,32 +55,31 @@ sudo apt install -y zsh git locales unzip \
     openjdk-8-jdk-headless openjdk-11-jdk-headless maven autojump \
     fonts-firacode inotify-tools jpegoptim \
     apt-transport-https ca-certificates gnupg libssl-dev \
-    &>> $LOGFILE
-
+    &>>$LOGFILE
 
 printf "📦 Install docker\n"
 sudo apt-get remove docker docker.io containerd runc
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 sudo apt update
 sudo apt intall -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Configure locale
 printf "🌍 Configure locales\n"
-localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8 &>> $LOGFILE
+localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8 &>>$LOGFILE
 
 # Clone dotfiles configuration
 printf "📦 Clone davidnussio/dotfiles from github\n"
 if [[ ! -d ~/dotfiles ]]; then
-    git clone --recursive https://github.com/davidnussio/dotfiles.git ~/dotfiles &>> $LOGFILE    
+    git clone --recursive https://github.com/davidnussio/dotfiles.git ~/dotfiles &>>$LOGFILE
 fi
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super><Shift>Page_Up']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super><Shift>Page_Down']"
 printf "📦 Init and update submodules\n"
-git submodule init &>> $LOGFILE
-git submodule update &>> $LOGFILE
+git submodule init &>>$LOGFILE
+git submodule update &>>$LOGFILE
 
 # Link zsh p10k theme
 ln -sfn ~/dotfiles/zsh-extra/powerlevel10k ~/dotfiles/zsh/.oh-my-zsh/themes/
@@ -87,37 +89,36 @@ ln -sfn ~/dotfiles/zsh-extra/zsh-z ~/dotfiles/zsh/.oh-my-zsh/plugins/
 
 # Install dotfiles
 printf "📦 Stow dotfiles: bash git\n"
-pushd ~/dotfiles &>> $LOGFILE
-rm ../.bash* ../.profile &>> $LOGFILE
-stow zsh bash git space-vim &>> $LOGFILE
-popd &>> $LOGFILE
+pushd ~/dotfiles &>>$LOGFILE
+rm ../.bash* ../.profile &>>$LOGFILE
+stow zsh bash git space-vim &>>$LOGFILE
+popd &>>$LOGFILE
 
 # Source bash profile
-reloadBashProfile &>> $LOGFILE
+reloadBashProfile &>>$LOGFILE
 
 printf "📦 Install python packages\n"
-pip3 install powerline-status powerline-gitstatus &>> $LOGFILE
-
+pip3 install powerline-status powerline-gitstatus &>>$LOGFILE
 
 # Install flatpak
 if [[ ! $(which flatpak) ]]; then
     sudo apt install -y flatpak
-    flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &>> $LOGFILE
+    flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &>>$LOGFILE
 fi
 
 # Install brew
 printf "📦 Install brew\n"
 if [[ ! -d "/home/linuxbrew/.linuxbrew" ]]; then
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash 
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
 fi
 
 # Install brew deps
-brew install gcc go &>> $LOGFILE
+brew install gcc go topgrade &>>$LOGFILE
 
 # Install github cli
 printf "📦 Github cli\n"
 if [[ ! $(which gh) ]]; then
-    brew install gh &>> $LOGFILE
+    brew install gh &>>$LOGFILE
 fi
 
 # Install space-vim (http://vim.liuchengxu.org/)
@@ -125,7 +126,7 @@ printf "📦 Install space-vim\n"
 if [[ -d $HOME/.config/nvim ]]; then
     printf "$HOME/.config/nvim already exists: SKIP\n"
 else
-    curl -fsSL https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh | bash -s -- --all &>> $LOGFILE
+    curl -fsSL https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh | bash -s -- --all &>>$LOGFILE
 
     pip3 install --upgrade pynvim
     pip3 install --upgrade msgpack
@@ -133,7 +134,7 @@ fi
 
 printf "🏢 Install GUI tools? ${INSTALL_DEV_GUI_TOOLS}\n"
 if [[ $INSTALL_DEV_GUI_TOOLS == 'y' ]]; then
-    sudo apt install -y gnome-tweak-tool ttf-mscorefonts-installer &>> $LOGFILE
+    sudo apt install -y gnome-tweak-tool ttf-mscorefonts-installer &>>$LOGFILE
     # Google cloud
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
@@ -166,17 +167,16 @@ if [[ $INSTALL_DEV_GUI_TOOLS == 'y' ]]; then
     # VPN
     printf "📦 openconnect\n"
     #sudo apt install -y openconnect network-manager-openconnect network-manager-openconnect-gnome &>> $LOGFILE
+    # Configure gnome
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Page_Up']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Page_Down']"
+    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Control><Super><Alt>Right']"
+    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Control><Super><Alt>Left']"
+    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super><Shift>Page_Up']"
+    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super><Shift>Page_Down']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "[]"
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "[]"
 fi
-
-# Configure gnome
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Page_Up']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Page_Down']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Control><Super><Alt>Right']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Control><Super><Alt>Left']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super><Shift>Page_Up']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super><Shift>Page_Down']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "[]"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "[]"
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
@@ -190,12 +190,12 @@ reloadBashProfile
 if [[ -e $(which snap) ]]; then
     # SNAP packages
     printf "📦 snap packages\n"
-    sudo snap install mdless &>> $LOGFILE
+    sudo snap install mdless &>>$LOGFILE
 fi
 
 # Install n for managing Node versions (using npm)
 printf "📦 Install n\n"
-curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$HOME/.fnm" --skip-shell &>> $LOGFILE
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$HOME/.fnm" --skip-shell &>>$LOGFILE
 
 # Source bash profile
 reloadBashProfile
@@ -205,11 +205,11 @@ sh <(curl -L https://nixos.org/nix/install)
 
 # Upgrade node
 printf "📦 Install Node LTS using n\n"
-curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "./.fnm" --skip-shell &>> $LOGFILE
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "./.fnm" --skip-shell &>>$LOGFILE
 
 # Install pnpm
 printf "📦 Install Node LTS using n\n"
-curl -fsSL https://get.pnpm.io/install.sh | PNPM_VERSION=7.0.0-rc.9 sh - &>> $LOGFILE
+curl -fsSL https://get.pnpm.io/install.sh | PNPM_VERSION=7.0.0-rc.9 sh - &>>$LOGFILE
 
 # Source bash profile
 reloadBashProfile
@@ -221,17 +221,16 @@ reloadBashProfile
 # Install some global packages
 printf "📦 Install global npm packages\n"
 pnpm add -g yarn nodemon npm-check-updates moleculer-cli diff-so-fancy jwt-cli \
-    esbuild-runner vsce serve neovim firebase-tools &>> $LOGFILE
+    esbuild-runner vsce serve neovim firebase-tools &>>$LOGFILE
 
 #  Note completion
-npm completion > ${HOME}/dotfiles/bash/.bash_completion.d/npm
+npm completion >${HOME}/dotfiles/bash/.bash_completion.d/npm
 
 # Install kubernetes tools
 curl "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" --output ${HOME}/.local/bin/kubectl
 chmod +x ${HOME}/.local/bin/kubectl
-kubectl completion bash > ${HOME}/dotfiles/bash/.bash_completion.d/kubectl
 
-# Print 
+# Print
 printf "✅ All done! \n"
 printf "👀 installation logfile ${LOGFILE}"
 printf "🏁 🏃 $ source ~/.bashrc\n"
--- scripts/update.sh
+++ scripts/update.sh
@@ -3,29 +3,32 @@
 # Ask for the administrator password upfront
 sudo -v
 
-printf "🌀 Ubuntu updates\n"
-sudo apt update >& /dev/null
-sudo apt full-upgrade -y >& /dev/null
+printf "🌀 Updates system\n"
+topgrade
+
+# sudo apt update >& /dev/null
+# sudo apt full-upgrade -y >& /dev/null
 
 #printf "🌀 n update\n"
 #n-update -y
 
-printf "🌀 Node update\n"
+# printf "🌀 Node update\n"
 #n lts
 #volta install node@lts
 
-printf "🌀 Npm update\n"
+# printf "🌀 Npm update\n"
 #npm install npm -g
 #npm update -g
 
-printf "🌀 Brew update\n"
-brew update
-brew upgrade
+# printf "🌀 Brew update\n"
+# brew update
+# brew upgrade
 
-printf "🌀 Flatpack update\n"
-flatpak update -y
+# printf "🌀 Flatpack update\n"
+# flatpak update -y
 
 # Update commands completion
 printf "🌀 Update completion files\n"
-npm completion > ~/dotfiles/bash/.bash_completion.d/npm
-fnm completions > ~/dotfiles/bash/.bash_completion.d/fnm
+npm completion >${HOME}/dotfiles/bash/.bash_completion.d/npm
+fnm completions >${HOME}/dotfiles/bash/.bash_completion.d/fnm
+kubectl completion bash >${HOME}/dotfiles/bash/.bash_completion.d/kubectl
