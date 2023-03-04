3120400bb75e455fbb6f7596e018e77291fd6546
David Nussio <david.nussio@gmail.com>
2023-03-04 15:34:37 +0100

    chore: removed update.sh, use topgrade instead

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    starship init fish | source
+  # Add brew path
+  fish_add_path -g /home/linuxbrew/.linuxbrew/bin
+
+  # Commands to run in interactive sessions can go here
+  starship init fish | source
+
+  # Abbreviations
+  abbr -a -- o xdg-open
 end
