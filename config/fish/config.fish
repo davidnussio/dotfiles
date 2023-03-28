1460dc1155b8215271f87ec0492c3c0fba04064e
David Nussio <david.nussio@gmail.com>
2023-03-28 16:29:06 +0200

    feat: speed up ... -> ../.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -41,6 +41,27 @@ if status is-interactive
   string match -q "$TERM_PROGRAM" "vscode"
   and . (code --locate-shell-integration-path fish)
 
+  # Speed up ... -> ../.
+  function expand-dot-to-parent-directory-path -d 'expand ... to ../.. etc'
+    # Get commandline up to cursor
+    set -l cmd (commandline --cut-at-cursor)
+
+    # Match last line
+    switch $cmd[-1]
+      case '*..'
+      commandline --insert '/.'
+      case '*'
+      commandline --insert '.'
+    end
+  end
+
+  function my_key_bindings
+    fish_default_key_bindings
+    bind . 'expand-dot-to-parent-directory-path'
+  end
+
+  set -g fish_key_bindings my_key_bindings
+
   # Load local fish config if it exists
   test -e ~/.config/fish/config-local.fish
   and . ~/.config/fish/config-local.fish
