4c5c82cd56a212029abc853437d7c2830af9fe66
David Nussio <david.nussio@gmail.com>
2024-07-04 16:07:59 +0200

    chore: gum

---
+++ features/gum/install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Template script for package management
+
+PACKAGE_NAME="gum"
+GUM_VERSION="0.14.1" # Use known good version
+
+isInstalled() {
+  if [[ $(which gum) ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+install() {
+  if isInstalled; then
+    echo "✅ $PACKAGE_NAME is already installed"
+    return
+  fi
+  elevateUser
+  cd /tmp
+  wget -O gum.deb "https://github.com/charmbracelet/gum/releases/latest/download/gum_${GUM_VERSION}_amd64.deb" &>> $LOGFILE
+  sudo apt install -y ./gum.deb &>> $LOGFILE
+  rm gum.deb &>> $LOGFILE
+  cd -
+}
+
+update() {
+  echo "Updating $PACKAGE_NAME"
+  # Add your update commands here
+}
+
+uninstall() {
+  if ! isInstalled; then
+    echo "✅ $PACKAGE_NAME is already uninstalled"
+    return
+  fi
+  elevateUser
+  sudo apt remove -y gum &>> $LOGFILE
+
+}
+
+. ./install/main.sh
