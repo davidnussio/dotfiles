3c357b7676460b80b07363bdee1efb28e11f495f
David Nussio <david.nussio@gmail.com>
2024-07-10 18:37:41 +0200

    chore: config

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,11 +1,15 @@
 # Configure watches
 elevateUser
-printf "🧬 Configure sysctl inotify\n"
+
 grep -q "fs.inotify.max_user_watches" /etc/sysctl.conf
 if [[ $? != 0 ]]; then
     echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf &>>$LOGFILE
     sudo sysctl -p &>>$LOGFILE
 fi
+
+[ ! -L $HOME/.local/bin/dotfiles-cli ] && ln -s $DOTFILES_PATH/bin/dotfiles-cli $HOME/.local/bin/dotfiles-cli
+
+gum confirm
 
 # Install gum before starting the setup
 source $DOTFILES_PATH/features/gum/install.sh install &>> /dev/null
